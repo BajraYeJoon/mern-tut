3489e472f7e28afbb7dd4eaf3cdf2050bc9c62ce
bajrayejoon <binaya.bajrashakya@gmail.com>
2022-12-20 15:41:09 +0530

    added async handler and custom error

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,35 +1,41 @@
 // //make controller function with const function name = (req, res) => {
 // res.status(200).json({ mesage})
 
-//
+//async handler for custom error handling during db connecction
+const asyncHandler = require("express-async-handler");
 
 ///@desc getTasks
 //@route  GET /API/TASKS
 //@access Private
-const getTasks = (req, res) => {
+const getTasks = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get task" });
-};
+});
 
 ///@desc setTasks
 //@route  POST /API/TASKS
 //@access Private
-const setTasks = (req, res) => {
+const setTasks = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("add text field");
+  }
+
   res.status(200).json({ message: "set task" });
-};
+});
 
 ///@desc updateTasks
 //@route  PUT /API/TASK/id
 //@access Private
-const putTasks = (req, res) => {
+const putTasks = asyncHandler(async (req, res) => {
   res.status(200).json({ mesage: `Update task ${req.params.id}` });
-};
+});
 
 ///@desc deleteTasks
 //@route  DELETE /API/TASKS
 //@access Private
-const delTasks = (req, res) => {
+const delTasks = asyncHandler(async (req, res) => {
   res.status(200).json({ mesage: `delete task ${req.params.id}` });
-};
+});
 
 module.exports = {
   getTasks,
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 //initialize the server using express
 const express = require("express");
 
+//bring in error handler
+const { errHandler } = require("./middleware/errorMiddleware");
+
 //initialize the dotenv for the environment variables
 const dotenv = require("dotenv").config();
 
@@ -10,7 +13,11 @@ const port = process.env.PORT || 3000;
 //make an app using the express server
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //middleware -use with the api endpoint and make a route folder with the routes that handles the crud
 app.use("/api/tasks", require("./routes/taskRoutes"));
+
+app.use(errHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
